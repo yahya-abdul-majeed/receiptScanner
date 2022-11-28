b76950ad79b53a35b7b3ea414a558571dac8f590
YahyaAbdulMajeed <yahya.zf2@gmail.com>
2022-11-28 12:34:45 +0700

    Added Text recognition with Mlkit using firebase

--- app/src/main/java/com/yahya/receiptapp/MainActivity.kt
+++ app/src/main/java/com/yahya/receiptapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.yahya.receiptapp
 
+import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.core.view.drawToBitmap
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.yahya.receiptapp.databinding.ActivityMainBinding
 import java.io.File
 
@@ -18,8 +24,10 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
     private val selectImageFromGalleryResult = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let { binding.imageViewReceipt.setImageURI(uri) }
+        this.uri = uri!!
     }
 
     private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSaved ->
@@ -44,11 +52,68 @@ class MainActivity : AppCompatActivity() {
             })
             takePicture.launch(uri)
         }
+        binding.btnExecute.setOnClickListener{
+            recognizeText()
+        }
+
     }
 
     private fun createImageFile(): File {
         val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile("temp_image",".jpg",storageDir)
+    }
+
+    private fun recognizeText(){
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        val image = InputImage.fromFilePath(this,uri);
+        val result = recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                binding.textView.text = visionText.text
+                // Task completed successfully
+                // [START_EXCLUDE]
+                // [START get_text]
+//                for (block in visionText.textBlocks) {
+//                    val boundingBox = block.boundingBox
+//                    val cornerPoints = block.cornerPoints
+//                    val text = block.text
+//
+//                    for (line in block.lines) {
+//                        // ...
+//                        for (element in line.elements) {
+//                            // ...
+//                        }
+//                    }
+//                }
+                // [END get_text]
+                // [END_EXCLUDE]
+            }
+            .addOnFailureListener { e ->
+
+            }
+
+
+    }
+
+    private fun processTextBlock(result: Text) {
+        // [START mlkit_process_text_block]
+        val resultText = result.text
+        for (block in result.textBlocks) {
+            val blockText = block.text
+            val blockCornerPoints = block.cornerPoints
+            val blockFrame = block.boundingBox
+            for (line in block.lines) {
+                val lineText = line.text
+                val lineCornerPoints = line.cornerPoints
+                val lineFrame = line.boundingBox
+                for (element in line.elements) {
+                    val elementText = element.text
+                    val elementCornerPoints = element.cornerPoints
+                    val elementFrame = element.boundingBox
+                }
+            }
+        }
+        // [END mlkit_process_text_block]
     }
 
 
